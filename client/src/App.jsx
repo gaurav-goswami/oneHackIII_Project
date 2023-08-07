dbde8c707d4489081e62869e90798abc43442036
domzymonz <domz.mi.monz@gmail.com>
2023-08-07 17:16:36 +0800

    reworked account pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,14 @@
 import './css/App.css'
 import './css/text.css'
 import Home from './pages/Home.jsx'
-import LogIn from './pages/LogIn.jsx'
-import SignUp from './pages/SignUp.jsx'
+import Connect from './pages/Authenticate.jsx'
 import {Route, Routes, Link} from 'react-router-dom'
 
 function App() {
   return (
     <>
       <Routes>
-        <Route exact path="/sign-up" element={<SignUp />} />
-        <Route exact path="/log-in" element={<LogIn />} />
+        <Route exact path="/connect" element={<Connect />} />
         <Route exact path="/" element={<Home />} />
       </Routes>
     </>
